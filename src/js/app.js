eb71680ae255e943064d8cba25b61461fcb4a039
Iggy Grey <github@pmphillips.ca>
2018-08-06 16:52:10 -0700

    Made Infura web3 provider

--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,7 @@ App = {
       App.web3Provider = web3.currentProvider;
     } else {
       // If no injected web3 instance is detected, fall back to Ganache
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      App.web3Provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/bf81f8cf57a54147b14231efb5bc641c');
     }
 
     web3 = new Web3(App.web3Provider);
